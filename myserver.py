290c71f5cbb1a9017f33170643c8f4b3ab470a4a
SuthepAku <suthep1234562002@gmail.com>
2024-06-30 05:06:39 +0700

    Add files via upload

---
+++ myserver.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Server is running!"
+
+def run():
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def server_on():
+    t = Thread(target=run)
+    t.start()
+
+
+server_on()
